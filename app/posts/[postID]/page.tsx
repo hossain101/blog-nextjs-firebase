6406f2f5211abdb59285d98ac559e8140022f516
Shoaib Hossain <shoaibhossain24@gmail.com>
2024-08-04 11:59:28 +0600

    chore: Add lazy loading for post images in page.tsx

---
+++ app/posts/[postID]/page.tsx
@@ -0,0 +1,36 @@
+import AuthorCard from "@/components/Posts/AuthorCard";
+import CategoryCard from "@/components/Posts/CategoryCard";
+import { getPost } from "@/lib/firebase/posts/read_server";
+import Image from "next/image";
+import React from "react";
+
+const page = async ({ params }: { params: { postID: string } }) => {
+  const post = await getPost(params.postID);
+
+  if (!post) {
+    return <div>Loading...</div>; // Or any other loading state representation
+  }
+
+  return (
+    <main className="flex justify-center">
+      <section className="p-10 flex flex-col gap-4 border max-w-[800px]">
+        <CategoryCard categoryId={post.categoryId} />
+        <h1 className="text-3xl font-bold">{post.title}</h1>
+        <Image
+          src={post.postImageUrl}
+          alt="post-image"
+          height={500}
+          width={500}
+          className="object-cover w-full"
+        />
+        <h5 className="text-xs text-gray-500">
+          {new Date(post.Timestamp).toLocaleDateString()}
+        </h5>
+        <AuthorCard authorId={post.authorId} />
+        <div dangerouslySetInnerHTML={{ __html: post.content }}></div>
+      </section>
+    </main>
+  );
+};
+
+export default page;
