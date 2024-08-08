ee6bf9a4018e5d5cb7285f9526240de16633371d
Shoaib Hossain <shoaibhossain24@gmail.com>
2024-08-08 12:31:02 +0600

    Refactor firebase configuration to use environment variables

--- lib/firebase.tsx
+++ lib/firebase.tsx
@@ -14,7 +14,7 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-console.log(process.env.AUTH_DOMAIN);
+
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
