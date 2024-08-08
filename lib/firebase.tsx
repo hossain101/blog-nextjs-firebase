917990bbcc84024dc7e278cc22449c98922da4ba
Shoaib Hossain <shoaibhossain24@gmail.com>
2024-08-08 12:24:30 +0600

    Update firebase configuration with environment variables

--- lib/firebase.tsx
+++ lib/firebase.tsx
@@ -14,16 +14,14 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-
+console.log(process.env.AUTH_DOMAIN);
 const firebaseConfig = {
-  apiKey: 'AIzaSyCmZC8gONvx_2UEERJ0hgQL0jCDK4w1KQY',
-  authDomain: "blog-tutorial-firebase.firebaseapp.com",
-  projectId: "blog-tutorial-firebase",
-  storageBucket: "blog-tutorial-firebase.appspot.com",
-  messagingSenderId: "782412734566",
-  appId: "1:782412734566:web:1eea35fee4e1b2b55f8da7",
-  measurementId: "G-E0KK0M2C8H",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
 // Initialize Firebase
